a2d4bde74c596b86ebfba9d4e3cc45c01497dc36
chriscoded <amaemechris@gmail.com>
2023-02-11 23:06:58 +0100

    11 Feb commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,15 +10,16 @@
       <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500">Register New Account</router-link>
     </p>
   </div>
+  
   <form class="mt-8 space-y-6" @submit="login" method="POST">
-    <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 my-1  bg-red-500 text-white rounded"> 
-      {{ errorMsg }} 
+    <Alert v-if="errorMsg">
+      {{ errorMsg }}
       <span @click="errorMsg = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-red-700">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </span>
-    </div>
+    </Alert>
     
     <input type="hidden" name="remember" value="true" />
     <div class="-space-y-px rounded-md shadow-sm">
@@ -26,6 +27,7 @@
         <label for="email-address" class="sr-only ">Email address</label>
         <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email" 
         class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+       
         <div v-if="errorMsg1" class="flex items-center justify-between py-3 px-5 my-1  bg-red-500 text-white rounded"> 
           {{ errorMsg1 }} 
           <span @click="errorMsg1 = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
@@ -64,10 +66,18 @@
     </div>
 
     <div>
-      <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <button :disabled="loading" type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      :class="{'cursor-not-allowed': loading, 'bg-indigo-100': loading, 'hover:bg-indigo-100': loading}"
+      >
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
           <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
         </span>
+
+        <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+      
         Sign in
       </button>
     </div>
@@ -77,6 +87,7 @@
 <script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 import store from '../store';
+import Alert from '../components/Alert.vue'
 import { useRouter } from 'vue-router';
 import { ref } from 'vue';
 
@@ -87,20 +98,24 @@ import { ref } from 'vue';
     password: '',
     remember: false
   }
+const loading = ref(false);
 let errorMsg = ref('');
 let errorMsg1 = ref('');
 
   function login(ev){
     ev.preventDefault();
 
+    loading.value = true; 
     store.dispatch('login', user)
       .then(() => {
+        loading.value = false; 
         router.push({
           name: 'Dashboard'
         })
       })
       .catch(err => {
-        errorMsg.value = err.response.data.error
+        loading.value = false; 
+        errorMsg.value = err.response.data.error;
         errorMsg1.value = err.response.data.message
       });
   }
